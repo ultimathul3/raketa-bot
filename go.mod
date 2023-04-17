e14fc1122b3b9bca5397d1e95c06decfa3cd195f
ultimathul3 <ultimathul3@ya.ru>
2023-04-17 19:15:34 +0300

    add receiving bot updates

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/vanyaio/raketa-bot
 
 go 1.19
 
-require github.com/ilyakaznacheev/cleanenv v1.4.2
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/ilyakaznacheev/cleanenv v1.4.2
+)
 
 require (
 	github.com/BurntSushi/toml v1.1.0 // indirect
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,9 +1,34 @@
 package app
 
 import (
+	"fmt"
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/vanyaio/raketa-bot/internal/config"
 )
 
 func Run(cfg *config.Config) {
+	bot, err := tgbotapi.NewBotAPI(cfg.BotToken)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	bot.Debug = cfg.BotDebug
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil {
+			if update.Message.Text == "/start" {
+				// TODO: sign up user
+				fmt.Println(update.Message.From.ID)
+			}
+		}
+	}
 }
