dd7d7361ac60f562800d0168cde3037ce49ebad9
ultimathul3 <ultimathul3@ya.ru>
2023-04-21 15:33:34 +0300

    add service client return

--- pkg/client/grpc.go
+++ pkg/client/grpc.go
@@ -3,15 +3,13 @@ package client
 import (
 	"fmt"
 
+	raketapb "github.com/vanyaio/raketa-backend/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-type GrpcClient struct {
-}
-
-func NewGrpcClient(serverIP string, serverPort uint16) (*GrpcClient, error) {
-	_, err := grpc.Dial(
+func NewGrpcClient(serverIP string, serverPort uint16) (raketapb.RaketaServiceClient, error) {
+	conn, err := grpc.Dial(
 		fmt.Sprintf("%s:%d", serverIP, serverPort),
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	)
@@ -19,6 +17,5 @@ func NewGrpcClient(serverIP string, serverPort uint16) (*GrpcClient, error) {
 		return nil, err
 	}
 
-	// TODO: return BotServiceClient
-	return nil, nil
+	return raketapb.NewRaketaServiceClient(conn), nil
 }
